0491a18392006ad075b8c8abd95e236e6aa3a5ae
kevin-wairi <kevinwairi@gmail.com>
2023-05-18 16:09:37 +0300

    assignment completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-
+    # getter
+    def name= (value)
+        @this_dogs_name = value
+    end
+    # setter
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "lassie"
+
+puts lassie.name
